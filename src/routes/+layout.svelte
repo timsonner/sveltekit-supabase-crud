af3242e236542dd8a5303e39883c381861b0df4e
timsonner <66705347+timsonner@users.noreply.github.com>
2022-10-16 01:00:03 -0600

    remove preload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <nav>
 	<a href="/">Home</a>
-	<a href="/shop" data-sveltekit-prefetch>Shop</a>
-	<a href="/commands" data-sveltekit-prefetch>Commands</a>
+	<a href="/shop">Shop</a>
+	<a href="/commands">Commands</a>
 	<a href="/what-is-my-user-agent">What's my user agent?</a>
+	<!-- data-sveltekit-prefetch -->
 </nav>
 <slot />
 
